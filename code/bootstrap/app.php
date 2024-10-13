0202d08193d6ccc999fb82359172dbf499fbc830
JFcoDiaz <jfcodiaz@gmail.com>
2024-10-12 20:35:10 -0600

    📄 [#24] Documentation and Force Provide application/json Always 🌐
    - Updated documentation to clearly state that responses will always be in JSON format.
    - Implemented middleware to ensure all API responses are `application/json`.

--- code/bootstrap/app.php
+++ code/bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \App\Http\Middleware\ForceJson::class,
         ]);
 
         $middleware->alias([
