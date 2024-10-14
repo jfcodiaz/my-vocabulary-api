9251a1682333d87073f714b5379a19fce5b7f6a7
JFcoDiaz <jfcodiaz@gmail.com>
2024-10-14 04:36:21 -0600

    🚀 [#28] documentation for my-vocabulary endpoint 📚
    - 📄 Added API documentation for my-vocabulary endpoint
    - 📝 Defined schemas for paginated WordUser data
    - 🔧 Improved clarity and structure in Swagger docs

--- code/app/Http/Controllers/Api/v1/Word/MyVocabularyController.php
+++ code/app/Http/Controllers/Api/v1/Word/MyVocabularyController.php
@@ -11,6 +11,52 @@ use App\Contracts\Repositories\IUserWordRepository;
 
 class MyVocabularyController extends Controller
 {
+    /**
+     * returns the paginated list of words that belong to the authenticated user
+     *
+     * @OA\Tag(
+     *   name="WordUser",
+     *   description="[My Vocabulary], List of words that belong to the authenticated user"
+     * )
+     * @OA\Schema(
+     *   schema="PaginatedWordUserSchema",
+     *   allOf={
+     *     @OA\Schema(ref="#/components/schemas/PaginatedResource"),
+     *   },
+     *   @OA\Property(
+     *     property="data",
+     *     type="array",
+     *     @OA\Items(ref="#/components/schemas/WordUserResource"),
+     *     description="Array of WordUserResource objects"
+     *   )
+     * )
+     *
+     * @OA\Get(
+     *   path="/api/v1/my-vocabulary",
+     *   summary="Get paginated WordUser records",
+     *   description="Returns paginated data for WordUserSchema.",
+     *   operationId="getPaginatedWordUsers",
+     *   tags={"WordUser"},
+     *   @OA\Response(
+     *     response=200,
+     *     description="Paginated WordUser data",
+     *     @OA\JsonContent(ref="#/components/schemas/PaginatedWordUserSchema")
+     *   ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated",
+     *         @OA\JsonContent(ref="#/components/schemas/UnauthenticatedResource")
+     *     ),
+     * )
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Contracts\Repositories\IUserWordRepository $userWordRepository
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function __invoke(Request $request, IUserWordRepository $userWordRepository): JsonResponse
     {
         $user = $request->user();
--- code/app/Http/Resources/Api/v1/Models/WordUserResource.php
+++ code/app/Http/Resources/Api/v1/Models/WordUserResource.php
@@ -8,7 +8,29 @@ class WordUserResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
+     * @OA\Schema(
+     *     schema="WordUserResource",
+     *     type="object",
+     *     title="WordUserResource",
+     *     @OA\Property(
+     *         property="word",
+     *         ref="#/components/schemas/WordResource"
+     *     ),
+     *     @OA\Property(
+     *         property="mine",
+     *         type="boolean",
+     *         description="Indicates if the word belongs to the authenticated user"
+     *     ),
+     *     @OA\Property(
+     *         property="created_at",
+     *         type="string",
+     *         format="date-time",
+     *         description="The date and time when the word was created"
+     *     )
+     * )
+     *
      * @param \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function toArray($request)
