f1c94bb32d4acf04485d64ddbb5b8049f42b22aa
JFcoDiaz <jfcodiaz@gmail.com>
2024-10-15 23:01:31 -0600

    📝 [#29] Add Swagger documentation for [POST] /api/v1/definition 📝
    - 📄 Created `CreateDefinitionSuccessfully` and `Definition` Swagger schemas.

--- code/app/Http/Controllers/Api/v1/Definitions/DefinitionStoreController.php
+++ code/app/Http/Controllers/Api/v1/Definitions/DefinitionStoreController.php
@@ -12,6 +12,81 @@ class DefinitionStoreController extends Controller
 {
     /**
      * Create a new definition.
+     * @OA\Post(
+     *     path="/api/v1/definitions",
+     *     summary="Store a new definition",
+     *     description="Create a new definition in the vocabulary API",
+     *     operationId="storeDefinition",
+     *     tags={"Definitions"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="wordId",
+     *                 type="integer",
+     *                 description="The term to be defined"
+     *             ),
+     *             @OA\Property(
+     *                property="wordTypeId",
+     *               type="integer",
+     *              description="The type of the term to be defined"
+     *           ),
+     *             @OA\Property(
+     *                 property="definition",
+     *                 type="string",
+     *                 description="The definition of the term",
+     *                 example="A word that is used to name a person, place, thing, or idea"
+     *             ),
+     *             @OA\Property(
+     *                property="verbBaseId",
+     *                type="integer",
+     *                description="The base form of the verb optional if wordTypeId is not a verb"
+     *           ),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Definition created successfully",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="id",
+     *                 type="integer",
+     *                 description="ID of the created definition"
+     *             ),
+     *             @OA\Property(
+     *                 property="term",
+     *                 type="string",
+     *                 description="The term that was defined"
+     *             ),
+     *             @OA\Property(
+     *                 property="definition",
+     *                 type="string",
+     *                 description="The definition of the term"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Some errors in request data",
+     *         @OA\JsonContent(ref="#/components/schemas/ErrorValidationResource")
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error"
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(ref="#/components/schemas/ForbiddenResource")
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated",
+     *        @OA\JsonContent(ref="#/components/schemas/UnauthenticatedResource")
+     *     )
+     * )
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\JsonResponse
--- code/app/Http/Resources/Api/v1/Controllers/Definitions/CreateDefinitionSuccessfullyResource.php
+++ code/app/Http/Resources/Api/v1/Controllers/Definitions/CreateDefinitionSuccessfullyResource.php
@@ -7,8 +7,35 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class CreateDefinitionSuccessfullyResource extends JsonResource
 {
     /**
+     *
+     * @OA\Schema(
+     *     schema="CreateDefinitionSuccessfully",
+     *     type="object",
+     *     title="CreateDefinitionSuccessfully",
+     *     properties={
+     *         @OA\Property(
+     *             property="success",
+     *             type="boolean",
+     *             description="Indicates if the operation was successful"
+     *         ),
+     *         @OA\Property(
+     *             property="message",
+     *             type="string",
+     *             description="A message describing the result"
+     *         ),
+     *         @OA\Property(
+     *             property="data",
+     *             @OA\Property(
+     *             type="object",
+     *                 property="definition",
+     *                 ref="#/components/schemas/DefinitionResource"
+     *             )
+     *         )
+     *     }
+     * )
      * @param  \Illuminate\Http\Request  $request
-     * @return array
+     *
+     * @return array<string, mixed>
      */
     public function toArray($request)
     {
